bd6fddd6d01ab1e4b008abdc548f75d19893d8ad
galuh wikri <galuhwikri05@gmail.com>
2024-12-03 20:43:35 +0700

    menyelesaikan news Page

---
+++ resources/views/post/news.blade.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.0/dist/cdn.min.js" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
+    <style>
+        html {
+            scroll-behavior: smooth;
+        }
+    </style>
+    <title>RaisinSoft</title>
+</head>
+<body class="bg-black text-gray-200 font-sans">
+
+    <!-- Navbar -->
+    <nav class="bg-black" x-data="{ isOpen: false }">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 items-center justify-between">
+                <div class="flex items-center">
+                    <a href="{{ route('home') }}" class="flex items-center">
+                        <img class="w-12 h-12 mr-3" src="/img/LOGO_RAISINSOFT3.png" alt="Your Company">
+                        <div class="text-xl font-semibold text-gray-100">RAISINSOFT</div>
+                    </a>
+                </div>
+                <div class="hidden md:flex items-center space-x-4">
+                    <!-- Navigation links -->
+                    <x-nav-link href="#games" class="text-white hover:text-gray-300">Game</x-nav-link>
+                    <x-nav-link href="" :active="request()->is('posts')" class="text-white hover:text-gray-300"><svg
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                    </x-nav-link>
+                    @if (!auth()->user())
+                        <details class="dropdown dropdown-end">
+                            <summary class="btn btn-link p-0 text-white hover:text-gray-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                </svg>
+                            </summary>
+                            <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 bg-[#242424]">
+                                <li>
+                                    <a href="{{ route('login') }}" :active="request() - > is('posts')">Login
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('register') }}" :active="request() - > is('posts')">Register
+                                    </a>
+                                </li>
+                            </ul>
+                        </details>
+                    @endif
+
+
+                    <!-- Profile dropdown -->
+                    <div class="relative ml-3">
+                        <div>
+                            @auth()
+                                <button type="button" @click="isOpen = !isOpen"
+                                    class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                    <span class="absolute -inset-1.5"></span>
+                                    <span class="sr-only">Open user menu</span>
+                                    <img class="h-8 w-8 rounded-full"
+                                        src="https://pics.craiyon.com/2024-09-01/V-vmRKOTQzSV2YsAR1rKEA.webp"
+                                        alt="galuh">
+                                </button>
+                            @endauth
+                        </div>
+                        <div x-show="isOpen" x-transition:enter="transition ease-out duration-100 transform"
+                            x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
+                            x-transition:leave="transition ease-in duration-75 transform"
+                            x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
+                            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
+                            tabindex="-1">
+                            <!-- Dropdown links -->
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                                tabindex="-1">Your Profile</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                                tabindex="-1">Settings</a>
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button type="submit" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                                    tabindex="-1">Log out</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mobile menu button -->
+                <div class="-mr-2 flex md:hidden">
+                    <button type="button" @click="isOpen = !isOpen"
+                        class="relative inline-flex items-center justify-center rounded-md bg-black p-2 text-gray-400 hover:bg-[#242424] hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="absolute -inset-0.5"></span>
+                        <span class="sr-only">Open main menu</span>
+                        <svg :class="{ 'hidden': isOpen, 'block': !isOpen }" class="block h-6 w-6" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"
+                            data-slot="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                        <svg :class="{ 'block': isOpen, 'hidden': !isOpen }" class="hidden h-6 w-6" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"
+                            data-slot="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div x-show="isOpen" class="md:hidden" id="mobile-menu">
+            <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                <x-nav-link href="#games"
+                    class="block text-gray-300 hover:bg-gray-700 hover:text-white"><svg
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </svg></x-nav-link>
+                <x-nav-link href="/posts" :active="request()->is('posts')"
+                    class="block text-gray-300 hover:bg-gray-700 hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                </svg></x-nav-link>
+            </div>
+            <div class="border-t border-gray-700 pb-3 pt-4">
+                <div class="flex items-center px-5">
+                    <div class="flex-shrink-0">
+                        <img class="h-10 w-10 rounded-full"
+                            src="https://pics.craiyon.com/2024-09-01/V-vmRKOTQzSV2YsAR1rKEA.webp" alt="#">
+                    </div>
+                    <div class="ml-3">
+                        <div class="text-base font-medium leading-none text-white">Galuh Wikri</div>
+                        <div class="text-sm font-medium leading-none mt-2 text-gray-400">Galuhwikri@gmail.com</div>
+                    </div>
+                </div>
+                <div class="mt-3 space-y-1 px-2">
+                    <a href="#"
+                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your
+                        Profile</a>
+                    <a href="#"
+                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button type="submit"
+                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Log
+                            out</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- News  -->
+    <div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+        <div class="absolute inset-0 -z-10 overflow-hidden">
+            <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+                aria-hidden="true">
+                <defs>
+                    <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1"
+                        patternUnits="userSpaceOnUse">
+                        <path d="M100 200V.5M.5 .5H200" fill="none" />
+                    </pattern>
+                </defs>
+                <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+                    <path
+                        d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+                        stroke-width="0" />
+                </svg>
+                <rect width="100%" height="100%" stroke-width="0"
+                    fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
+            </svg>
+        </div>
+        <div
+            class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+            <div
+                class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                <div class="lg:pr-4">
+                    <div class="lg:max-w-lg">
+                        <p class="text-base/7 font-semibold text-black">Trending</p>
+                        <h1 class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                            PS5 vs Xbox Series X: Pertarungan Konsol Generasi Terbaru</h1>
+                        <p class="mt-6 text-xl/8 text-gray-700">Di era gaming modern, dua raksasa konsol, PlayStation 5
+                            (PS5) dan Xbox Series X, bersaing ketat untuk memikat hati para gamer. Kedua konsol ini
+                            membawa lompatan besar dalam teknologi, performa, dan pengalaman bermain. Berikut adalah
+                            perbandingan mendalam antara PS5 dan Xbox Series X untuk membantu Anda menentukan mana yang
+                            lebih cocok dengan kebutuhan gaming Anda.</p>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+                <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+                    src="https://www.notebookcheck.nl/fileadmin/Notebooks/News/_nc4/immortals-FSR-3-support-1.jpg"
+                    alt="">
+            </div>
+            <div
+                class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                <div class="lg:pr-4">
+                    <div class="max-w-xl text-base/7 text-gray-700 lg:max-w-lg">
+                        <p><strong class="font-semibold text-gray-900">PlayStation 5</strong>
+                        <p>PS5 dari Sony menawarkan prosesor AMD Zen 2 dan GPU RDNA 2 yang mampu menjalankan game
+                            hingga resolusi 4K dengan frame rate hingga 120 FPS. Konsol ini juga diperkuat dengan SSD
+                            ultra-cepat yang meminimalkan waktu loading secara drastis, memberikan pengalaman yang lebih
+                            mulus dalam game seperti Spider-Man: Miles Morales..</p>
+                        <p><strong class="font-semibold text-gray-900">XBOX Series X</strong>
+                        <p>Xbox Series X hadir sebagai konsol terkuat Microsoft, menawarkan spesifikasi serupa dengan
+                            PS5 namun dengan GPU sedikit lebih unggul. Xbox Series X mampu menjalankan game hingga
+                            resolusi 8K, memberikan pengalaman grafis yang luar biasa pada game seperti Forza Horizon 5.
+                            Selain itu, konsol ini memiliki penyimpanan internal yang besar dan kompatibilitas game
+                            lintas generasi.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</html>
